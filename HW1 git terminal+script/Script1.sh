b8a4dd8de8fd856f524aac20c0c75e837a7eeda9
unknown <a.sakovich1997@gmail.com>
2023-02-22 15:02:42 +0300

    first

---
+++ HW1 git terminal+script/Script1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd firstHWTerminal
+mkdir script1 script2 script3
+cd skript1
+touch script1.txt script2.txt script3.txt script1.json script2.json
+mkdir script12 script13 script14
+ls
+mv script1.txt script2.txt C:/Users/asako/git/test/
